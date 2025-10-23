8df0ae1073ebcafc4fc4cb4ea7232084fdff9d66
Mohith <tatinenimohith@gmail.com>
2025-10-23 22:50:29 +0530

    Initial commit

---
+++ frontend/src/DashBoard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>University Management</h1>      
+      <button onClick={unive}>Add University</button>
+
+    </div>
+  )
+}
+
+export default Dashboard;
